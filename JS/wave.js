9ed3b8134d0db1a81cf1214504631975c74d0f4e
Preben Aas <prebenaas@gmail.com>
2020-08-10 19:35:31 +0200

    Redesigned landing page

---
+++ JS/wave.js
@@ -0,0 +1,98 @@
+// Based on pen by Varun Vachhar: https://codepen.io/winkerVSbecks/pen/EVJGVj
+
+var path = document.querySelector("#wave");
+var animation = document.querySelector("#moveTheWave");
+var m = 0.512286623256592433;
+
+function buildWave(w, h) {
+  var a = h / 4;
+  var y = h / 2;
+
+  var pathData = [
+    "M",
+    w * 0,
+    y + a / 2,
+    "c",
+    a * m,
+    0,
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+    "s",
+    -(1 - a) * m,
+    a,
+    a,
+    a,
+    "s",
+    -(1 - a) * m,
+    -a,
+    a,
+    -a,
+  ].join(" ");
+
+  path.setAttribute("d", pathData);
+}
+
+buildWave(90, 60);
